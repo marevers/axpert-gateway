28bfadd73172ab90cf70092980c938804a8b071a
Martijn Evers <mevers@gk-software.com>
2025-08-15 18:49:03 +0200

    Start implementation of the command API

--- axpert.go
+++ axpert.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/marevers/energia/pkg/axpert"
+	"github.com/marevers/energia/pkg/connector"
 )
 
 // Initialise any inverters connected through USB and return them.
@@ -30,4 +32,64 @@ func initInverters() ([]*Inverter, error) {
 	}
 
 	return invs, nil
+}
+
+// Sets the output source priority to either 'utility', 'solar' or 'sbu'.
+func setOutputSourcePriority(c connector.Connector, p string) error {
+	var osp axpert.OutputSourcePriority
+
+	switch p {
+	case "utility":
+		osp = axpert.OutputUtilityFirst
+	case "solar":
+		osp = axpert.OutputSolarFirst
+	case "sbu":
+		osp = axpert.OutputSBUFirst
+	default:
+		return fmt.Errorf("error: unrecognized output source priority: %s", p)
+	}
+
+	err := axpert.SetOutputSourcePriority(c, osp)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Sets the charger source priority to either 'utilityfirst', 'solarfirst', 'solarandutility' or 'solar only'.
+func setChargerSourcePriority(c connector.Connector, p string) error {
+	var csp axpert.ChargerSourcePriority
+
+	switch p {
+	case "utilityfirst":
+		csp = axpert.ChargerUtilityFirst
+	case "solarfirst":
+		csp = axpert.ChargerSolarFirst
+	case "solarandutility":
+		csp = axpert.ChargerSolarAndUtility
+	case "solaronly":
+		csp = axpert.ChargerSolarOnly
+	default:
+		return fmt.Errorf("error: unrecognized charger source priority: %s", p)
+	}
+
+	err := axpert.SetChargerSourcePriority(c, csp)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Sets the maximum AC charge current.
+func setMaxACChargeCurrent(c connector.Connector, cr uint8) error {
+	// TODO: Add some kind of boundaries here
+
+	err := axpert.SetMaxUtilityChargingCurrent(c, cr)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- routes.go
+++ routes.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	log "github.com/sirupsen/logrus"
 )
+
+// CommandRequest represents the JSON body for control API commands
+type CommandRequest struct {
+	Value string `json:"value"`
+}
+
+// CommandResponse represents the JSON response for control API commands
+type CommandResponse struct {
+	Command string `json:"command"`
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
 
 func (a *Application) Routes() http.Handler {
 	router := httprouter.New()
@@ -16,6 +30,7 @@ func (a *Application) Routes() http.Handler {
 
 	router.Handler(http.MethodGet, *metricsPath, promhttp.HandlerFor(a.Prometheus.Reg, promhttp.HandlerOpts{}))
 	router.HandlerFunc(http.MethodGet, "/healthz", func(w http.ResponseWriter, r *http.Request) { http.Error(w, "OK", http.StatusOK) })
+	router.HandlerFunc(http.MethodPost, "/api/command/:command", a.handleCommand)
 	router.HandlerFunc(http.MethodGet, "/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`<html>
 		<head><title>axpert-gateway</title></head>
@@ -28,4 +43,42 @@ func (a *Application) Routes() http.Handler {
 	})
 
 	return router
+}
+
+// handleCommand handles control API commands
+func (a *Application) handleCommand(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+	command := params.ByName("command")
+
+	// Check if control API is enabled
+	if !*controlEnabled {
+		http.Error(w, "Control API is disabled", http.StatusForbidden)
+		return
+	}
+
+	// Parse JSON body
+	var req CommandRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		log.Errorf("Failed to decode request body: %v", err)
+		http.Error(w, "Invalid JSON body", http.StatusBadRequest)
+		return
+	}
+
+	log.Infof("Received command: %s with value: %s", command, req.Value)
+
+	response := CommandResponse{
+		Command: command,
+		Status:  "success",
+		Message: "Command received and processed",
+	}
+
+	// TODO: Implement actual command processing logic here
+	// For now, just return a success response
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		log.Errorf("Failed to encode response: %v", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
 }
